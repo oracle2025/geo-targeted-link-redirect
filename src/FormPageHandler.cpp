63137f0eed50bb2544667a5be340b7e6021e2469
Richard Spindler <richard.spindler@gmail.com>
2023-01-25 23:15:14 +0100

    wip

--- src/FormPageHandler.cpp
+++ src/FormPageHandler.cpp
@@ -20,14 +20,18 @@
 #include <vector>
 #include <iostream>
 #include "FormPageHandler.hpp"
+extern char form_html_start[] asm( "_binary_form_html_start" );
+extern char form_html_end[]   asm( "_binary_form_html_end" );
+extern size_t form_html_size  asm( "_binary_form_html_size" );
 
 void FormPageHandler::handleRequest(Poco::Net::HTTPServerRequest &request, Poco::Net::HTTPServerResponse &response) {
     response.setChunkedTransferEncoding(true);
     response.setContentType("text/html");
     ostream& responseStream = response.send();
-    ifstream fin("../src/form.html", ios_base::in | ios_base::binary);
+    //ifstream fin("../src/form.html", ios_base::in | ios_base::binary);
+    string html_template(form_html_start, form_html_end);
     ostringstream oss;
-    oss << fin.rdbuf();
+    oss << html_template;//fin.rdbuf();
     string content(oss.str());
     content = regex_replace( content, regex("@LINK1_URL@"), Mongoose::Utils::htmlEntities(request.get("link1", "") ));
     content = regex_replace( content, regex("@LINK2_URL@"), Mongoose::Utils::htmlEntities(request.get("link2", "") ));
